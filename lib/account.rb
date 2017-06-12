ab7436a84d6c971e34913c0bfa5b31ed809ed9ea
sophieklm <sklm@live.co.uk>
2017-06-12 12:24:09 +0100

    adds error handling

--- lib/account.rb
+++ lib/account.rb
@@ -7,11 +7,20 @@ def initialize
 end
 
 def deposit(amount)
+  catch_error(amount)
   @balance += amount
 end
 
 def withdraw(amount)
+  catch_error(amount)
+  fail "Withdrawal exceeds account balance" if (@balance - amount) < 0
   @balance -= amount
+end
+
+private
+
+def catch_error(amount)
+  fail "Cannot process a negative amount" if amount < 0
 end
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -14,16 +14,29 @@ describe Account do
     it 'increases the balance by the amount specified' do
       expect{subject.deposit(10)}.to change{subject.balance}.by(10)
     end
+
+    it 'only allows a positive amount to be added' do
+      expect{subject.deposit(-10)}.to raise_error "Cannot process a negative amount"
+    end
   end
 
   describe '#withdraw' do
     before do
       account.deposit(50)
     end
-    
+
     it 'decreases the balance by the amount specified' do
       expect{subject.withdraw(10)}.to change{subject.balance}.by(-10)
     end
+
+    it 'only allows a positive amount to be withdrawn' do
+      expect{subject.withdraw(-10)}.to raise_error "Cannot process a negative amount"
+    end
+
+    it 'only cannot withdraw more than current balance' do
+      expect{subject.withdraw(60)}.to raise_error "Withdrawal exceeds account balance"
+    end
+
   end
 
 end
